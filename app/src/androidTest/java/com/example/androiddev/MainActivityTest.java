eb75e3c790a110001b66c818b9c6bd7eb8a25a01
Dylan Eastridge <theillusionofthegift>
2021-04-19 17:33:50 -0700

    .texts fix

--- app/src/androidTest/java/com/example/androiddev/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiddev/MainActivityTest.java
@@ -5,13 +5,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -21,9 +15,4 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
 
-    @Test
-    public void hasTextOnScreen(){
-        onView(withId(R.id.textview_first))
-                .check(matches(withText(R.string.hello_world)));
-    }
 }
