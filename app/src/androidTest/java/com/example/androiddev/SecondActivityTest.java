5e33c1498a77184ed5cf846657f215aed5ea6419
Dylan Eastridge <theillusionofthegift>
2021-05-11 19:58:35 -0700

    added tests

--- app/src/androidTest/java/com/example/androiddev/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/androiddev/SecondActivityTest.java
@@ -2,23 +2,37 @@ package com.example.androiddev;
 
 
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
 
+import androidx.test.espresso.Root;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 
+import static android.service.autofill.Validators.not;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 public class SecondActivityTest {
@@ -29,7 +43,7 @@ public class SecondActivityTest {
 
 
     @Test
-    public void checkNavDrawerMatches() {
+    public void checkNavDrawerMatches() throws InterruptedException {
         // Open Drawer to click on navigation.
         onView(withId(R.id.drawer_layout))
                 .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
@@ -37,6 +51,41 @@ public class SecondActivityTest {
 
         onView(withId(R.id.nav_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_matches));
+
+        onView(withId(R.id.drawer_layout))
+                .perform(DrawerActions.close()); // Close Drawer
+
+        onView(withId(R.id.recycler_view)).perform(
+                RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id. like_button)));
+
+        Thread.sleep(1500);
+
+        onView(withText(R.string.TOAST_STRING)).inRoot(withDecorView(not(is(getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+
+    }
+
+    // Convenience helper
+    public static class MyViewAction {
+
+        public static ViewAction clickChildViewWithId(final int id) {
+            return new ViewAction() {
+                @Override
+                public Matcher<View> getConstraints() {
+                    return null;
+                }
+
+                @Override
+                public String getDescription() {
+                    return "Click on a child view with specified id.";
+                }
+
+                @Override
+                public void perform(UiController uiController, View view) {
+                    View v = view.findViewById(id);
+                    v.performClick();
+                }
+            };
+        }
 
     }
 
