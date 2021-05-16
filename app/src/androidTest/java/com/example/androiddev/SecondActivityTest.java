1f0a20af5a5f61717313ffa26be4a106b839bab1
Dylan Eastridge <theillusionofthegift>
2021-05-15 19:46:00 -0700

    fix tests for tost? maybe

--- app/src/androidTest/java/com/example/androiddev/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/androiddev/SecondActivityTest.java
@@ -2,17 +2,23 @@ package com.example.androiddev;
 
 
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 
+import androidx.test.espresso.Root;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,6 +26,7 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
@@ -45,10 +52,10 @@ public class SecondActivityTest {
         onView(withId(R.id.drawer_layout))
                 .perform(DrawerActions.close()); // Close Drawer
 
-//        onView(withId(R.id.recycler_view)).perform(
-//                RecyclerViewActions.actionOnItemAtPosition(1, MyViewAction.clickChildViewWithId(R.id. like_button)));
-
-
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id.like_button)));
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id.like_button)));
+        onView(withText(R.string.mssage)).inRoot(new ToastMatcher())
+                .check(matches(isDisplayed()));
     }
 
     // Convenience helper
@@ -121,6 +128,27 @@ public class SecondActivityTest {
 
         onView(allOf(withId(R.id.profile))).check((matches(withText("Your Profile!"))));
 
+    }
+
+    public class ToastMatcher extends TypeSafeMatcher<Root> {
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("is toast");
+        }
+
+        @Override
+        public boolean matchesSafely(Root root) {
+            int type = root.getWindowLayoutParams().get().type;
+            if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+                IBinder windowToken = root.getDecorView().getWindowToken();
+                IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                if (windowToken == appToken) {
+                    return true;
+                }
+            }
+            return false;
+        }
 
     }
 }
